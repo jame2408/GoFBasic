2679406d389b9592c9eed20f407b067a2ad066d8
jame2408 <jame2408@gmail.com>
2017-03-26 13:38:41 +0800

    Singleton : Add test case 「default_instance_is_not_null」 and pass test.

---
+++ GoFBasic/Singleton/FileSystem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoFBasic.Singleton
+{
+    public class FileSystem
+    {
+        public static object getInstance()
+        {
+            return new FileSystem();
+        }
+    }
+}
---
+++ GoFBasic/Singleton/FileSystemTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GoFBasic.Singleton
+{
+    [TestClass]
+    public class FileSystemTest
+    {
+        [TestMethod]
+        public void default_instance_is_not_null()
+        {
+            var fs1 = FileSystem.getInstance();
+            Assert.IsNotNull(fs1);
+        }
+    }
+}
